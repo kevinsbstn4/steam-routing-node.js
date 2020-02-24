e5bc4249723c2fe04ad1332b548600a87663a9e6
kevinsbstn4 <kevincaesar22@gmail.com>
2020-02-24 14:00:07 +0700

    index

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+var port = 3000
+    app.use(express.static('public'))
+
+    app.get('/', (req, res) => {
+        res.sendfile('index.html')
+    })
+  
+    app.listen(port, () => {
+            console.log(`App berjalan pada port ${port}`)
+    })
